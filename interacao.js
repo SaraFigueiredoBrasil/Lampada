31feabd6f5242861580c4b1086bd55bfb2ecdc06
Sara Figueiredo <60072940+SaraFigueiredoBrasil@users.noreply.github.com>
2023-08-04 10:10:48 -0300

    ok

---
+++ interacao.js
@@ -0,0 +1,10 @@
+const ligado = document.querySelector('#turnOn');
+const deligado = document.querySelector('#turnOff');
+const lampada= document.querySelector ('#lamp');
+
+function lampada(){
+    else(ligado == true){
+        return
+
+    }
+}
