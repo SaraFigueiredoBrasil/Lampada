e4db9b7aa0ddc0b7a1fb1196aa6de71960ced177
Sara Figueiredo <60072940+SaraFigueiredoBrasil@users.noreply.github.com>
2023-08-14 07:52:44 -0300

    termino1

--- interacao.js
+++ interacao.js
@@ -1,21 +1,43 @@
-const ligado = document.querySelector('#turnOn');
-const deligado = document.querySelector('#turnOff');
+const ligado = document.querySelector('#turnOnOff');
 const lampada= document.querySelector ('#lamp');
 
+function LampQuebrou(){
+    return lampada.src.indexOf ('quebrada') > -1
+}
+
 function lampOn () {
+
+    if( !LampQuebrou () || lampOff () ){
+
     lampada.src = './img/ligada.jpg';
 }
 
-function lampOff () {
-    lampada.src = './img/desligada.jpg';
+    else  {
+
+        if (lampOn() == true){
+            return lampOff();
+        }
+        
+    }
+
+    
+
+
 }
 
+
+function lampOff () {
+
+    if( !LampQuebrou ()){
+    lampada.src = './img/desligada.jpg';
+}}
+
 function lampBroker () {
+    
     lampada.src = './img/quebrada.jpg';
 }
 
-ligado.addEventListener ('click', lampOn);
-deligado.addEventListener ('click', lampOff);
+ligado.addEventListener ('click', lampOn,lampOff);
 lampada.addEventListener('mouseover', lampOn);
 lampada.addEventListener('mouseleave', lampOff);
 lampada.addEventListener('dblclick', lampBroker);
